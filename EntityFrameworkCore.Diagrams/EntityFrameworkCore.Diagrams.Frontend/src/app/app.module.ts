b2cf90a0186212816a661b26f69918309741cb65
EvAlex <evalexhimself@gmail.com>
2017-07-14 21:28:40 +0300

    display entity properties

--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/app.module.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/timer';
+
+import 'hammerjs';
 
 import { AppComponent } from './components/app/app.component';
 import { DbDiagramComponent } from './components/db-diagram/db-diagram.component';
@@ -24,7 +28,8 @@ import { ClrTypeComponent } from './components/clr-type/clr-type.component';
         BrowserModule,
         HttpModule,
         BrowserAnimationsModule,
-        MaterialModule
+        MaterialModule,
+        CdkTableModule
     ],
     providers: [ApiService],
     bootstrap: [AppComponent],
--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/components/db-entity-diagram-figure/db-entity-diagram-figure.component.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/components/db-entity-diagram-figure/db-entity-diagram-figure.component.ts
@@ -1,20 +1,63 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import { DataSource, CollectionViewer } from '@angular/cdk';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { DbEntity } from '../../models/db-entity';
+import { DbEntityProperty } from '../../models/db-entity-property';
 
 @Component({
     selector: 'efd-db-entity-diagram-figure',
     templateUrl: './db-entity-diagram-figure.component.html',
     styleUrls: ['./db-entity-diagram-figure.component.scss']
 })
-export class DbEntityDiagramFigureComponent implements OnInit {
+export class DbEntityDiagramFigureComponent implements OnInit, OnChanges {
 
     @Input()
     entity: DbEntity;
 
-    constructor() { }
+    entityContext = new DbEntityContext();
+    propertiesDataSource = new DbEntityPropertiesDataSource(this.entityContext);
+    displayedColumns = ['name', 'clrType'];
+
+    constructor(private readonly _changeDetector: ChangeDetectorRef) {
+    }
 
     ngOnInit() {
+        setTimeout(() => {
+            this._changeDetector.detectChanges();
+        });
+    }
+
+    ngOnChanges(changes) {
+        this.entityContext.entity = this.entity;
+    }
+
+}
+
+class DbEntityContext {
+    dataChange: BehaviorSubject<DbEntityProperty[]> = new BehaviorSubject<DbEntityProperty[]>([]);
+
+    set entity(value: DbEntity) {
+        this._entity = value;
+        if (this._entity instanceof DbEntity) {
+            this.dataChange.next(this._entity.properties);
+        }
+    }
+    private _entity: DbEntity = null;
+}
+
+export class DbEntityPropertiesDataSource extends DataSource<DbEntityProperty> {
+    constructor(private readonly _entityContext: DbEntityContext) {
+        super();
+    }
+
+    connect(collectionViewer: CollectionViewer): Observable<DbEntityProperty[]> {
+        return this._entityContext.dataChange;
+    }
+
+    disconnect(collectionViewer: CollectionViewer): void {
+        //  do nothing
     }
 
 }
