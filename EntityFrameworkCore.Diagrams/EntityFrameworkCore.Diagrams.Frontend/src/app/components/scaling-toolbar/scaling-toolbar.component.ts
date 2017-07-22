a77314cf93e57beeef68a82474de79efb64bc1ff
EvAlex <evalexhimself@gmail.com>
2017-07-22 10:26:17 +0300

    scaffold scaling-toolbar

---
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/components/scaling-toolbar/scaling-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'efd-scaling-toolbar',
+  templateUrl: './scaling-toolbar.component.html',
+  styleUrls: ['./scaling-toolbar.component.scss']
+})
+export class ScalingToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
