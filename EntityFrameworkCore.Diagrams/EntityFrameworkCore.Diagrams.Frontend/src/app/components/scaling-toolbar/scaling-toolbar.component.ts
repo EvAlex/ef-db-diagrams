2f0086095a9ee14497170955967ef6ae138b66ec
EvAlex <evalexhimself@gmail.com>
2017-07-22 10:52:24 +0300

    implement scaling toolbar

--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/components/scaling-toolbar/scaling-toolbar.component.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/components/scaling-toolbar/scaling-toolbar.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'efd-scaling-toolbar',
-  templateUrl: './scaling-toolbar.component.html',
-  styleUrls: ['./scaling-toolbar.component.scss']
+    selector: 'efd-scaling-toolbar',
+    templateUrl: './scaling-toolbar.component.html',
+    styleUrls: ['./scaling-toolbar.component.scss']
 })
 export class ScalingToolbarComponent implements OnInit {
 
-  constructor() { }
+    @Input()
+    defaultScale = 1;
 
-  ngOnInit() {
-  }
+    @Input()
+    minScale = 0.1;
+
+    @Input()
+    maxScale = 10;
+
+    @Input()
+    scale: number = null;
+
+    @Output()
+    scaleChange = new EventEmitter<number>();
+
+    constructor() { }
+
+    ngOnInit() {
+        if (this.scale === null) {
+            this.scale = this.defaultScale;
+        }
+    }
+
+    zoomIn() {
+        this.scale += this.getStep();
+        this.scaleChange.emit(this.scale);
+    }
+
+    zoomOut() {
+        this.scale -= this.getStep();
+        this.scaleChange.emit(this.scale);
+    }
+
+    resetZoom() {
+        this.scale = this.defaultScale;
+        this.scaleChange.emit(this.scale);
+    }
+
+    private getStep(): number {
+        return this.scale <= this.defaultScale
+            ? (this.defaultScale - this.minScale) / 20
+            : (this.maxScale - this.defaultScale) / 20;
+    }
 
 }
