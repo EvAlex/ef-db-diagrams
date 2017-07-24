08561658f777c6a83959cbddd99d508e56145847
EvAlex <evalexhimself@gmail.com>
2017-07-24 11:30:12 +0300

    improve relations connectors directions choosing

--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/models/db-entity-relation-connector.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/models/db-entity-relation-connector.ts
@@ -4,4 +4,10 @@ import { Point } from './point';
 export class DbEntityRelationConnector {
     lines: Line[] = [];
     externalPoint: Point;
+    // directionOverride: Direction | null = null;
+}
+
+export enum Direction {
+    LeftToRight,
+    RightToLeft
 }
