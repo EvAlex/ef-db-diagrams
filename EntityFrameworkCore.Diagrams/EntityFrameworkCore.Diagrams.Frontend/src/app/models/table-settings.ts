068f56426bc89b236fd8115bdd0f053486501d11
EvAlex <evalexhimself@gmail.com>
2017-07-26 12:57:10 +0300

    implement reordering columns

--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/models/table-settings.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src/app/models/table-settings.ts
@@ -28,4 +28,40 @@ export class TableSettings {
     get displayedColumnsKeys() {
         return this.columns.filter(e => e.visible).map(e => e.key);
     }
+
+    canMoveColumnUp(column: ColumnSettings) {
+        this.assertForeignColumn(column);
+        const index = this.columns.indexOf(column);
+        return index > 0;
+    }
+
+    moveColumnUp(column: ColumnSettings) {
+        if (!this.canMoveColumnUp(column)) {
+            throw new Error('Cannot move column up. It is already at the top.');
+        }
+        const index = this.columns.indexOf(column);
+        this.columns.splice(index, 1);
+        this.columns.splice(index - 1, 0, column);
+    }
+
+    canMoveColumnDown(column: ColumnSettings) {
+        this.assertForeignColumn(column);
+        const index = this.columns.indexOf(column);
+        return index < this.columns.length - 1;
+    }
+
+    moveColumnDown(column: ColumnSettings) {
+        if (!this.canMoveColumnDown(column)) {
+            throw new Error('Cannot move column down. It is already at the bottom.');
+        }
+        const index = this.columns.indexOf(column);
+        this.columns.splice(index, 1);
+        this.columns.splice(index + 1, 0, column);
+    }
+
+    private assertForeignColumn(column: ColumnSettings) {
+        if (this.columns.indexOf(column) === -1) {
+            throw new Error('Specified column does not belong to current TableSettings instance');
+        }
+    }
 }
