e0fd76b4c0183f329a85c940a3b9b177bbb66fbd
EvAlex <evalexhimself@gmail.com>
2017-08-01 10:57:33 +0200

    setup libs

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDDSseuMfdgCFfztSsnAhCIgi-itsfPEvM",
+        authDomain: "db-diagrams.firebaseapp.com",
+        databaseURL: "https://db-diagrams.firebaseio.com",
+        projectId: "db-diagrams",
+        storageBucket: "db-diagrams.appspot.com",
+        messagingSenderId: "290073738630"
+    }
 };
