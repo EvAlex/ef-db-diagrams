e40a5ec255fba8c738c54a0e2cda2d5d2103f022
EvAlex <evalexhimself@gmail.com>
2017-08-05 11:37:24 +0200

    add promo text

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -2,15 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+import { firebaseConfig } from './firebase-config';
 
 export const environment = {
     production: false,
-    firebase: {
-        apiKey: "AIzaSyDDSseuMfdgCFfztSsnAhCIgi-itsfPEvM",
-        authDomain: "db-diagrams.firebaseapp.com",
-        databaseURL: "https://db-diagrams.firebaseio.com",
-        projectId: "db-diagrams",
-        storageBucket: "db-diagrams.appspot.com",
-        messagingSenderId: "290073738630"
-    }
+    firebase: firebaseConfig
 };
