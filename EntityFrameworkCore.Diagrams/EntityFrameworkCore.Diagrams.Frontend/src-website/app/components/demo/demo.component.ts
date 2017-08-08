b6a5288cfc7b97c5bd3b4c52563c22818ccbbee0
EvAlex <evalexhimself@gmail.com>
2017-08-08 11:24:26 +0200

    demo page & deploy

--- EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src-website/app/components/demo/demo.component.ts
+++ EntityFrameworkCore.Diagrams/EntityFrameworkCore.Diagrams.Frontend/src-website/app/components/demo/demo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../../../src/environments/environment';
+
 @Component({
     selector: 'efd-demo',
     templateUrl: './demo.component.html',
@@ -8,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class DemoComponent implements OnInit {
 
     fullscreen = false;
+
+    commitSha = environment.versions.revision;
+    commitLink = `${environment.versions.repository}commit/${environment.versions.revision}`;
 
     constructor() { }
 
